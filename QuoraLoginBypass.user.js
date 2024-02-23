855c850e1e261caa190ee513cdd9342903efdfbd
glob_bruh <cirethe101@gmail.com>
2024-02-22 20:56:12 -0700

    Script rework and dark mode button

--- QuoraLoginBypass.user.js
+++ QuoraLoginBypass.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        NoQuoraLogin
 // @description Allows you to view Quora pages without being prompted to login or sign up.
-// @version     1.3
+// @version     2.0
 // @icon        logo.png
 // @grant       none
-// @run-at      document-start
+// @run-at      document-end
 // @match       https://www.quora.com/*
 // @namespace   https://github.com/
 // @downloadURL https://github.com/glob-bruh/NoQuoraLogin/raw/main/QuoraLoginBypass.user.js
@@ -23,26 +23,41 @@ const whitelistURL = [
     "https://www.quora.com/profile/*"
 ]
 
-var url = window.location.href;
-var changeURL = true;
-for (let i = 0, constLen = whitelistURL.length ; i < constLen ; i++) {
-    if (whitelistURL[i].slice(-1) == "*") {
-        if (RegExp(whitelistURL[i]).test(url)) {
-            changeURL = false
-        }
-    } else {
-        if (url == whitelistURL[i]) {
-            changeURL = false
+// Figure out how to wait until the page is fully
+// loaded before (with run-at:document-start) executing this.
+function AddElement(tag, text) {
+    var x = document.createElement(tag);
+    var y = document.createTextNode(text)
+    x.appendChild(y)
+    var z = document.getElementsByClassName("q-flex")[4]
+    z.appendChild(x)
+}
+
+function ShareCheck() {
+    var url = window.location.href;
+    var changeURL = true;
+    for (let i = 0, constLen = whitelistURL.length ; i < constLen ; i++) {
+        if (whitelistURL[i].slice(-1) == "*") {
+            if (RegExp(whitelistURL[i]).test(url)) {
+                changeURL = false
+            }
+        } else {
+            if (url == whitelistURL[i]) {
+                changeURL = false
+            }
         }
     }
-}
-if (changeURL == false || url.indexOf("share=1") != -1) {
-    return false
-} else {
-    if (url.indexOf("?") != -1) {
-        url += "&share=1";
+    if (changeURL == false || url.indexOf("share=1") != -1) {
+        return false
     } else {
-        url += "?share=1";
+        if (url.indexOf("?") != -1) {
+            url += "&share=1";
+        } else {
+            url += "?share=1";
+        }
+        window.location.replace(url)
     }
-    window.location.replace(url)
 }
+
+ShareCheck()
+AddElement("button", "Change to dark mode")
